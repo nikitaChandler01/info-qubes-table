4102795f7a67a7b1e9b1c449436c3205b26e4f92
TLIQ <12932894+TLIQ@users.noreply.github.com>
2023-11-14 12:38:15 +0300

    Parameters page

---
+++ src/pages/ParametersPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <h3>Parameters</h3>
+</template>
+
+<script setup>
+
+// type.name.group - группа, остальное - параметры
+// вывести отдельно таблицу параметров, отдельно групп
+
+
+
+</script>
+
+
+<style scoped>
+
+</style>
